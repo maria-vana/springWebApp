2a2220353b0112303f677635fbd74e8b99383303
MGYORGY <maria.gyorgy@computacenter.com>
2023-05-22 14:27:57 +0300

    first commit

---
+++ src/test/java/guru/springframework/springSwebapp/SpringSwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springSwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
